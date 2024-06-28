31b7299c5088b33b1d44c0e0fa7ce37377558e77
Laszlo-Caballero <laszlocaballero@gmail.com>
2024-06-28 17:26:32 -0500

    Datos

--- main.py
+++ main.py
@@ -9,16 +9,21 @@ class MainPanel(Tk):
         self.title("Cajero")
         self.geometry("900x500")
         self.config(bg="white")
+        
         self.ImgAñadirOpen = Image.open("images/agregar-usuario.png") 
         self.ImgAñadir = ImageTk.PhotoImage(self.ImgAñadirOpen)
         self.BtnAñadir = ttk.Button(self, text ="Añadir Cliente" , image = self.ImgAñadir, compound=TOP, style='Rounded.TButton')
         self.BtnAñadir.image = self.ImgAñadir
         self.BtnAñadir.pack()
+        
         self.ImgCashOpen = Image.open("images/cash-report.png")
         self.ImgCash = ImageTk.PhotoImage(self.ImgCashOpen)
         self.BtnCash = ttk.Button(self, text ="Actualizar Dispensador" , image = self.ImgCash, compound=TOP, style='Rounded.TButton')
         self.BtnCash.image = self.ImgCash
         self.BtnCash.pack()
+
+
+        self.BtnUsuario = [] 
         
 
 def Main():

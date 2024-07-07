358fcaceb1a8d82e0518bba07b860f3f6f58600e
Laszlo-Caballero <125536074+Laszlo-Caballero@users.noreply.github.com>
2024-07-06 22:54:17 -0500

    Solucion

--- Components/Prueba.py
+++ Components/Prueba.py
@@ -1,15 +1,14 @@
-import tkinter as ttk
+from tkinter import ttk, TOP
 from PIL import Image, ImageTk
 
 class Prueba(ttk.Button):
-    def __init__(self, parent, text, image_path, *args, **kwargs):
-        self.font = ('Arial', 12)
+    def __init__(self, parent, text, image_path):
         self.text = text
         self.image_path = image_path
         
-        super().__init__(parent, *args, **kwargs)
+        super().__init__(parent)
         
-        self.config(font=self.font, text=self.text, compound=ttk.LEFT, padx=10, pady=10)
+        self.config(text=self.text, compound=TOP, style='Rounded.TButton')
         
         image = Image.open(image_path)
         image = image.resize((32, 32))

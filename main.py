61763c724cfeda6fca6fa391f663b9dcf5c37054
Laszlo-Caballero <125536074+Laszlo-Caballero@users.noreply.github.com>
2024-07-06 22:41:15 -0500

    Añadir Cajero

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from tkinter import *
+from tkinter import messagebox, Tk, Frame, StringVar, TOP, Toplevel
 from tkinter import ttk
 from PIL import Image, ImageTk
 from Class.Cajero import Cajero
@@ -6,7 +6,8 @@ import pickle
 import py_hot_reload
 from Interface.InterfazAñadir import InterfazAñadir
 from Interface.InterfazUsuario import Cuenta
-from Interface.InterfazActualizarDepostio import InterfazActualizar
+from Interface.InterfazActualizarDepostio import InterfazActualizarDepostio
+from Interface.InterfazAñadirCajero import AñadirCajero
 
 
 class MainPanel(Tk):
@@ -21,39 +22,40 @@ class MainPanel(Tk):
         with open('Data/Cajero.pkl', 'rb') as file:
             self.Cajeros : list[Cajero] = pickle.load(file) 
 
-        self.btnFrame = Frame(self, bg='white')
-        self.btnFrame.pack(expand=True)
+        self.FramePrincipal = Frame(self, bg='white')
+        self.FramePrincipal.pack(expand=True)
+        
+        self.txtVariableOpc = StringVar()
+        self.CmbCajeros = ttk.Combobox(self.FramePrincipal, textvariable=self.txtVariableOpc, state='readonly')
+        
+        self.listaCajeros = []
+
+        for cajero in self.Cajeros:
+            self.listaCajeros.append(cajero.Sucursal)
+        self.listaCajeros.append("Agregar Cajero")
+        
+        self.CmbCajeros['values'] = self.listaCajeros
+        self.CmbCajeros.bind("<<ComboboxSelected>>", self.on_Change_ComboBox)
+
+        self.CmbCajeros.pack(pady=(0,20))
+
+        self.btnFrame = Frame(self.FramePrincipal, bg='white')
+        self.btnFrame.pack()
 
         self.ImgAñadirOpen = Image.open("images/agregar-usuario.png") 
         self.ImgAñadir = ImageTk.PhotoImage(self.ImgAñadirOpen)
         self.BtnAñadir = ttk.Button(self.btnFrame, text ="Añadir Usuario" , image = self.ImgAñadir, compound=TOP, style='Rounded.TButton', command=self.AbrirVentanaAgregar)
         self.BtnAñadir.image = self.ImgAñadir
         self.BtnAñadir.pack(side='left')
-        
-        self.FrameMid = Frame(self.btnFrame, bg='white')
 
-        self.txtVariableOpc = StringVar()
-        self.CmbCajeros = ttk.Combobox(self.FrameMid, textvariable=self.txtVariableOpc, state='readonly')
-        
-        listaCajeros = []
 
-        for cajero in self.Cajeros:
-            listaCajeros.append(cajero.Sucursal)
-        
-        self.CmbCajeros['values'] = listaCajeros
-        self.CmbCajeros.bind("<<ComboboxSelected>>", self.on_Change_ComboBox)
-
-        self.CmbCajeros.pack()
 
         self.ImgCashOpen = Image.open("images/cash-report.png")
         self.ImgCash = ImageTk.PhotoImage(self.ImgCashOpen)
-        self.BtnCash = ttk.Button(self.FrameMid, text ="Actualizar Dispensador" , image = self.ImgCash, compound=TOP, style='Rounded.TButton', command=self.AbrirVentanaActualizar)
+        self.BtnCash = ttk.Button(self.btnFrame, text ="Actualizar Dispensador" , image = self.ImgCash, compound=TOP, style='Rounded.TButton', command=self.AbrirVentanaActualizar)
         self.BtnCash.image = self.ImgCash
-        self.BtnCash.pack(pady=(20,0))
+        self.BtnCash.pack(side='left', padx=50)
 
-        
-
-        self.FrameMid.pack(side='left', padx=50)
 
         self.ImgUsuarioOpen = Image.open("images/usuario.png")
         self.ImgUsuario = ImageTk.PhotoImage(self.ImgUsuarioOpen)
@@ -61,25 +63,38 @@ class MainPanel(Tk):
         self.BtnUsuario.image = self.ImgUsuario
         self.BtnUsuario.pack(side='right')
         
-
         
 
     def AbrirVentanaAgregar(self):
         InterfazAñadir(self)
 
     def AbrirVentanaActualizar(self):
-        InterfazActualizar(self)
-
+        if self.selecCajero != -1:
+            InterfazActualizarDepostio(self, self.selecCajero)
+        else:
+            messagebox.showerror("Error", "Tienes que elejir un cajero de alguna sucursal")
+    
     def AbrirVentanaUsuario(self):
          Cuenta(self)
 
     def on_Change_ComboBox(self, event):
         select = self.txtVariableOpc.get()
-
         for i in range(len(self.Cajeros)):
             if self.Cajeros[i].Sucursal == select:
-                self.selecCajero = i 
+                self.selecCajero = i
+        if(select == "Agregar Cajero"):
+            top = AñadirCajero(self, self.Cajeros)
+            top.protocol("WM_DELETE_WINDOW", lambda: self.on_toplevel_close(top))
+
         print(self.selecCajero)
+    
+    def on_toplevel_close(self, top: Toplevel):
+        self.listaCajeros = []
+        for cajero in self.Cajeros:
+            self.listaCajeros.append(cajero.Sucursal)
+        self.listaCajeros.append("Agregar Cajero")
+        self.CmbCajeros['values'] = self.listaCajeros
+        top.destroy()
 
 def Main():
     app = MainPanel()

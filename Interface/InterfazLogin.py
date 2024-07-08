911aa0dc158c96aa3b233f17f24c1d39b435b708
luciana-31 <lgogny74@gmail.com>
2024-07-07 20:12:10 -0500

    Login

--- Interface/InterfazLogin.py
+++ Interface/InterfazLogin.py
@@ -1,6 +1,7 @@
 from tkinter import Toplevel, Label, Frame, Entry, Button
 from PIL import Image, ImageTk, ImageFont
 from tkinter import font
+from tkinter import ttk
 
 
 class Login(Toplevel):
@@ -24,20 +25,39 @@ class Login(Toplevel):
         frame_inicio = Frame(self, width = 470, height = 400, background='white')
         frame_inicio.pack(side="right", padx=40, pady=10)
         
+        imageEntry = Image.open("./images/entry_img.png")
+        imageEntry = imageEntry.resize((250, 50))
+        self.image_entry = ImageTk.PhotoImage(imageEntry)
+        
         inicio_label = Label(frame_inicio, text="Banca por Internet", foreground="#003399", background='white',font=self.FontDmi)
-        inicio_label.place(x=160, y=25)
+        inicio_label.place(x=145, y=25)
         
         user_label = Label(frame_inicio, text="Numero de la Tarjeta", foreground="#003399",background='white', font=(self.FontDmi,12))
         user_label.place(x=60, y=100)
         
-        user_entry =Entry(frame_inicio, width=50)
-        user_entry.place(x=60, y=150)
+        entryUser_label = Label(frame_inicio, image = self.image_entry, bd=0)
+        entryUser_label.image = self.image_entry
+        entryUser_label.place(x=110, y=135)
+        
+        user_entry =Entry(frame_inicio, width=35, bd=0)
+        user_entry.place(x=120, y=150)
         
         pass_label = Label(frame_inicio, text="Contraseña", foreground="#003399",background='white', font=(self.FontDmi,12))
         pass_label.place(x=60, y=200)
         
-        pass_entry =Entry(frame_inicio, width=50)
-        pass_entry.place(x=60, y=250)
+        entryPass_label = Label(frame_inicio, image = self.image_entry, bd=0)
+        entryPass_label.image = self.image_entry
+        entryPass_label.place(x=110, y=230)
+        
+        pass_entry = Entry(frame_inicio, width=35, bd = 0)
+        pass_entry.place(x=120, y=245)
+        
+        btnContinuar = Button(frame_inicio, text="Continuar", fg="white", bg="#F36F2C", borderwidth=0, relief="flat", width=30, font=(self.FontDmi,12))
+        btnContinuar.place(x=98, y=300)
+        
+            
+        
+        
 
         
         
